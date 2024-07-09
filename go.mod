91902562ffaae741eb1f3a9fe141755017f0e136
Tuz <hi@cmdong.com>
2024-07-09 20:57:52 +0800

    migrate to go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module game24
+
+go 1.20
+
+require github.com/novalagung/golpal v1.0.0
--- vendor/github.com/novalagung/golpal/install_cli.bat
+++
@@ -1,8 +0,0 @@
-:: Golpal - Easy to use Golang Eval Library
-:: Created by Noval Agung Prayogo <caknopal@gmail.com>
-:: http://novalagung.com/
-
-go install
-
-cd cli
-go build -a -o %GOPATH%\bin\golpal.exe
